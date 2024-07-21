4383edc9eb3a52bacc6cdb3ebc5946a7b42e5f8d
BounAbdallah <bigmabo61345@gmail.com>
2024-07-21 15:40:43 +0000

    Ajout de la configuration a la base de donnees

---
+++ app.js
@@ -0,0 +1,3 @@
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1eXBkc3ZudmV4bGRxbGR1anV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExMzA0NjcsImV4cCI6MjAzNjcwNjQ2N30.UmYRinhMa5_GtUIU9au9GCQJboY_sD2vEPaRv7bTjns";
+const url = "https://suypdsvnvexldqldujuy.supabase.co";
+const database = supabase.createClient(url, key);
